5729389e365340aa5488485130e87e986b4978a9
Oğuzhan Seleker <seleker17@itu.edu.tr>
2022-03-31 20:59:56 +0300

    course controller

---
+++ Web Client/DYS.WebClient/Controllers/CourseController.cs
@@ -0,0 +1,25 @@
+﻿using DYS.WebClient.Services;
+using DYS.WebClient.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using SharedLibrary.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DYS.WebClient.Controllers
+{
+    public class CourseController : BaseController
+    {
+        private readonly ILessonService _lessonService;
+        public CourseController(IUserService userService, ISharedIdentityService sharedIdentityService, ILessonService lessonService) : base(userService, sharedIdentityService)
+        {
+            _lessonService = lessonService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+
+        }
+    }
+}
