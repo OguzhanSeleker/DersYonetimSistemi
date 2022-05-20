44c6b2998081ee6b1c3ba6c4819860453e865aa6
Oğuzhan Seleker <seleker17@itu.edu.tr>
2022-05-20 09:10:00 +0300

    bugfix

--- Services/RabbitMqConsumer/Services.RabbitMQ.Consumer/Program.cs
+++ Services/RabbitMqConsumer/Services.RabbitMQ.Consumer/Program.cs
@@ -57,18 +57,10 @@ namespace Services.RabbitMQ.Consumer
                     services.AddOptions<MassTransitHostOptions>()
                         .Configure(options =>
                         {
-                            // if specified, waits until the bus is started before
-                            // returning from IHostedService.StartAsync
-                            // default is false
                             options.WaitUntilStarted = true;
-
-                            // if specified, limits the wait time when starting the bus
                             options.StartTimeout = TimeSpan.FromSeconds(10);
-
-                            // if specified, limits the wait time when stopping the bus
                             options.StopTimeout = TimeSpan.FromSeconds(30);
                         });
-                    //services.AddHostedService<Worker>();
                 });
     }
 }
